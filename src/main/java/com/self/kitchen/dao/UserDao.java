55c826195e1b7cc9f4e612c06f835a8d51ee7d90
Deng Ya Jun <1648195295@qq.com>
2019-10-30 22:21:37 +0800

    亚军

--- src/main/java/com/self/kitchen/dao/UserDao.java
+++ src/main/java/com/self/kitchen/dao/UserDao.java
@@ -4,6 +4,7 @@ package com.self.kitchen.dao;
 import com.self.kitchen.dto.UserDto;
 
 
+import com.self.kitchen.entity.Food;
 import com.self.kitchen.entity.User;
 import org.apache.ibatis.annotations.ResultType;
 import org.apache.ibatis.annotations.Select;
@@ -19,4 +20,22 @@ public interface UserDao {
     /*@Select("select * from user")
     @ResultType(User.class)
     User loginUser();*/
+
+    @Select("select * from user where username = #{username}")
+    @ResultType(Integer.class)
+    User selectUserByUsername(String username);
+
+    @Select("insert into user(username, password) values(#{username}, #{password})")
+    @ResultType(Integer.class)
+    void save(String username, String password);
+
+    /*@Select("select f.food_name,m.* from food f inner join (user u, mariable m) on(u.id=f.uid, f.id=m.fid) " +
+            "where u.id=#{uid}")
+    @ResultType(Integer.class)
+    Map<String, List<Mariable>> selectVegetableByCaiPin(int uid);*/
+
+
+    @Select("select food_name from user where username = #{username}")
+    @ResultType(Integer.class)
+    Food selectFoodByUid(int uid);
 }
--- src/main/java/com/self/kitchen/service/UserService.java
+++ src/main/java/com/self/kitchen/service/UserService.java
@@ -3,6 +3,7 @@ package com.self.kitchen.service;
 
 
 import com.self.kitchen.dto.UserDto;
+import com.self.kitchen.entity.User;
 import com.self.kitchen.vo.ResultVo;
 
 public interface UserService {
@@ -11,4 +12,12 @@ public interface UserService {
     ResultVo loginOut(String username);
 
     ResultVo userMessage(String username);
+
+    ResultVo register(String username, String password);
+
+    User selectUserByUsername(String username);
+
+    ResultVo validateCode(String code);
+
+    void save(String username, String password);
 }
